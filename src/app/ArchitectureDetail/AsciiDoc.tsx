c0f61c07a997023f8b5e3b5a99132f31b834b5b8
Christina Lin <clin@redhat.com>
2022-02-22 11:57:05 -0500

    Adding youtube iframe for ADOC #16

--- src/app/ArchitectureDetail/AsciiDoc.tsx
+++ src/app/ArchitectureDetail/AsciiDoc.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Asciidoctor from 'asciidoctor.js/dist/browser/asciidoctor';
+import { NimblrIcon } from '@patternfly/react-icons';
 
+//video::rPQoq7ThGAU[youtube]
+//<iframe width="560" height="315" src="https://www.youtube.com/embed/XXXX" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
 export default class Asciidoc extends React.PureComponent {
     static propTypes = {
         className: PropTypes.string,
@@ -20,7 +23,16 @@ export default class Asciidoc extends React.PureComponent {
         let pattern = /<li><a href=\"#/g;
         let result = pattern.test(convertedhtml);
         //console.log("result-->"+result);
+        
         var filteredhtml = convertedhtml.replace(pattern,"<li><a href=\""+window.location+"#");
+
+        filteredhtml = filteredhtml.replace(pattern, "")
+        //let youtubepattern = /video::(\w+)\[youtube\]/g;
+        
+        //Replace video::XXXX[youtube]
+        filteredhtml=filteredhtml.replace('video::','<div class="youtubeframe"><p><iframe src="https://www.youtube.com/embed/');
+        filteredhtml=filteredhtml.replace('[youtube]','" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe></p></div>');
+           
         //console.log("filteredhtml-->"+filteredhtml);
         return (
             
