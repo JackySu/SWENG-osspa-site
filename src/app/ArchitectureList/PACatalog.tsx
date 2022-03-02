b41bfcfcb077a407887838e0117f20fb520f6347
Christina Lin <clin@redhat.com>
2022-03-02 13:47:48 -0500

    Fix for #22 Remove links in card, remove hover over on card, only link via image and title

--- src/app/ArchitectureList/PACatalog.tsx
+++ src/app/ArchitectureList/PACatalog.tsx
@@ -47,13 +47,13 @@ class PACatalog extends React.Component {
                 {
                   tempdisplay.map( item =>
                   <GalleryItem key={item.ppid}>
-                    <Link to={DETAIL_URL+'?ppid='+item.ppid} ><Card isHoverable key={item.ppid} isCompact={true} >
+                   <Card isHoverable={false} key={item.ppid} isCompact={true} >
                       <CardHeader>
                         <CardHeaderMain>
-                        <Brand src={CARD_IMG_URL+item.Image1Url} alt="Card Image" style={{ width: '500px' }} />
+                        <Link to={DETAIL_URL+'?ppid='+item.ppid} ><Brand src={CARD_IMG_URL+item.Image1Url} alt="Card Image" style={{ width: '500px' }} /></Link>
                         </CardHeaderMain>
                       </CardHeader>
-                      <CardTitle>{item.Heading}</CardTitle>
+                      <Link to={DETAIL_URL+'?ppid='+item.ppid} ><CardTitle>{item.Heading}</CardTitle></Link>
                       <CardBody>{item.Summary}</CardBody>
                       <CardFooter>
                         <LabelGroup numLabels={20}>
@@ -71,7 +71,7 @@ class PACatalog extends React.Component {
                               )}
                               </LabelGroup>
                         </CardFooter>
-                    </Card></Link>
+                    </Card>
                   </GalleryItem>
                 )}
           </Gallery>
