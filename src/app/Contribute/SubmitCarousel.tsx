81c7ecdac40cf7bb1313c9345bb35f4d3b9c9528
Christina Lin <clin@redhat.com>
2022-03-28 13:23:57 -0400

    fixed #28 adjust font size, center body in carousel, updating the latest PA list

--- src/app/Contribute/SubmitCarousel.tsx
+++ src/app/Contribute/SubmitCarousel.tsx
@@ -4,6 +4,7 @@ import {
 
 import { css } from '@patternfly/react-styles';
 import '@app/react-asciidoc/fedora.css';
+import '@app/react-asciidoc/carousel.css';
 import { Carousel } from '@trendyol-js/react-carousel';
 
   
@@ -22,30 +23,32 @@ class SubmitCarousel extends React.Component {
       <div class="submitCarousel">
         <Carousel show={3} slide={1} swiping={true} infinite={false}>
         <div>
-          <h4>Submit Idea</h4>
+          <div class="title_carousel">Submit Idea</div>
           <Brand src="/architect/portfolio/images/contribute_process_01.png" alt="Submit process 1" width="200px"/>
-                  Users share how they implemented multi-product solutions by following our design template, and submitting an issue in Gitlab.
+          <p>
+              Users share how they implemented multi-product solutions by following our design template, and submitting an issue in Gitlab.
+          </p>
         </div>
         <div>
-            <h4>Review</h4>
+            <div class="title_carousel">Review</div>
             <Brand src="/architect/portfolio/images/contribute_process_02.png" alt="Submit process 2" width="200px"/>
                 <p>
                   A Red Hat Portfolio Architect will review your submission and determine where it fits within our portfolio.
                 </p>      
             </div><div>
-            <h4>Feedback</h4>
+            <div class="title_carousel">Feedback</div>
                   <Brand src="/architect/portfolio/images/contribute_process_03.png" alt="Submit process 3" width="200px"/>
                 <p>
                   Once reviewed, the Architect will connect with feedback on what they’ve determined, and if necessary how to refactor to complete the architecture.
                   </p>      
             </div><div>
-            <h4>Life Cycle</h4>
+            <div class="title_carousel">Life Cycle</div>
                   <Brand src="/architect/portfolio/images/contribute_process_04.png" alt="4" width="200px"/>
                 <p>
                   As the architecture is being developed, you will work with your Architect to standardize this solution.
                   </p>      
             </div><div>
-            <h4>Publishing</h4>
+            <div class="title_carousel">Publishing</div>
                   <Brand src="/architect/portfolio/images/contribute_process_05.png" alt="Card Image" width="200px"/>
                 <p>
                   The Architecture is ready for marketing and publishing on Red Hat channels.
