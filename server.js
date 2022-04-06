4f2bd958834a370da11aa29767d8b9a0921ab41b
Christina Lin <clin@redhat.com>
2022-04-06 12:40:16 -0400

    #34 Generate template XML base on host

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const {createProxyMiddleware} = require("http-proxy-middleware");
 app.use(express.static(__dirname + '/asset'));
 app.use(history());
 
+
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "dist/architect/portfolio")));
   
@@ -26,11 +27,12 @@ if(process.env.NODE_ENV === "production"){
 }
 
 app.get('/api',function(req,res){  
+    //console.log(req.hostname);
     res.send('<p>This is a api Data</p>');  
 }); 
-const root = express();
-const contextPath='/architect/portfolio';  
-root.use(contextPath, app)
+//const root = express();
+//const contextPath='/architect/portfolio';  
+//root.use(contextPath, app)
 
 app.use(
   "/osspa",
@@ -139,4 +141,41 @@ app.use(
 
 
 
-app.listen(PORT, () => console.log('Portfolio Public Site listening on port 8081!'))
+app.listen(PORT, () => console.log('Portfolio Public Site listening on port', PORT))
+
+
+// Dynamically Generate the Template Tools
+const { create } = require('xmlbuilder2');
+const fs = require('fs');
+var os = require("os");
+var os_hostname = os.hostname();
+
+var HOSTNAME = "www.redhat.com";
+if(os_hostname=="portfolio-wnix-us" && PORT==8081){
+  HOSTNAME="dev.osspa.org"
+}else if(os_hostname=="portfolio-wnix-us"){
+  HOSTNAME="osspa.org"
+}else if(!os_hostname.startsWith("pawebsite")){
+  HOSTNAME="localhost:"+PORT;
+}
+
+const xml_root = create({ version: '1.0' })
+  .ele('templates')
+    .ele('template', { section: 'Red Hat Templates' ,url: 'https://'+HOSTNAME+'/architect/portfolio/Templates/DetailDiagram.xml',title:'Detail Diagram',preview:'https://'+HOSTNAME+'/architect/portfolio/Templates/DetailDiagram.png',libs:''})
+    .up()
+    .ele('template', { section: 'Red Hat Templates' ,url: 'https://'+HOSTNAME+'architect/portfolio/Templates/LogicalDiagram.xml',title:'Logical Diagram',preview:'https://'+HOSTNAME+'/architect/portfolio/Templates/LogicalDiagram.png',libs:''})
+    .up()
+    .ele('template', { section: 'Red Hat Templates' ,url: 'https://'+HOSTNAME+'/architect/portfolio/Templates/SchematicDiagram.xml',title:'Schematic Diagram',preview:'https://'+HOSTNAME+'/architect/portfolio/Templates/SchematicDiagram.png',libs:''})
+    .up()
+    .ele('template', { section: 'Red Hat Templates' ,url: 'https://'+HOSTNAME+'/architect/portfolio/Templates/RedHatAllAssets.xml',title:'All Diagrams',preview:'https://'+HOSTNAME+'/architect/portfolio/Templates/RedHatAllAssets.png',libs:''})
+  .up();
+
+
+
+// convert the XML tree to string
+const xml = xml_root.end({ prettyPrint: true });
+let full_file_name = "./asset/architect/portfolio/tool/template.xml";
+fs.writeFileSync(full_file_name, xml, function(err) {
+    if (err) throw err;
+});
+1
